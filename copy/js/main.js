ca4171c97048557eb10cd53a41e6802e72573dc8
lovehakumai <masaotomeza@gmail.com>
2024-08-25 14:03:56 +0900

    copy finish

---
+++ copy/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    // Hamburger menu
+
+    $('.toggle_btn').on('click',function(){
+        if($(header).hasClass('open')){
+            $('#header').removeClass('open');
+        }else{
+            $('#header').addClass('open');
+        }
+    });
+
+    $('#mask').on('click', function(){
+        $('#header').removeClass('open');
+    });
+});
